37441125540cf41abb22c9d2c8858444349a6e48
jcgonzalezsanchez <54146783+jcgonzalezsanchez@users.noreply.github.com>
2019-11-05 21:26:01 -0500

    Load 05/11/2019 21:25

--- TeamSeguros/TS.WebApp/wwwroot/js/site.js
+++ TeamSeguros/TS.WebApp/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+
+// Or with jQuery
+
+$(document).ready(function () {
+    $('.datepicker').datepicker();
+    $('select').formSelect();
+});
